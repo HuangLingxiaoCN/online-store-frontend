d1e72ed09e7be1c26545fb4024331119dc53ba02
Lingxiao Huang <jasonhlx1213@gmail.com>
2022-02-09 00:22:53 +0200

    checkout button will empty cart items

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,19 +3,23 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
 
 import CartList from "../components/UI/cart/CartList";
-import "../sass/ShoppingCart.scss";
 import { Footer } from "../components/footer/Footer";
+import { RootState } from "../redux/Types";
+import "../sass/ShoppingCart.scss";
 
 export default function CartPage() {
   // For updating UI
   const [cartItems, setCartItems] = useState<any>([]);
   const [totalPrice, setTotal] = useState(0);
+  const userEmail = useSelector((state: RootState) => state.email);
   const navigate = useNavigate();
 
+  // Get user cart
   // Can't perform a React state update on an unmounted component.
   useEffect(() => {
     // get all cart items from the user
@@ -35,6 +39,7 @@ export default function CartPage() {
     };
   }, []);
 
+  // calculate total price of cart
   useEffect(() => {
     let total = 0;
     cartItems.forEach((i: any) => {
@@ -43,8 +48,22 @@ export default function CartPage() {
     setTotal(total);
   }, [cartItems]);
 
+  const checkoutHandler = () => {
+    axios
+      .patch(
+        "https://fierce-spring-store-backend.herokuapp.com/api/user/cart/clear",
+        {
+          email: userEmail,
+        }
+      )
+      .catch((err) => console.error(err));
+    navigate("/order", { state: cartItems });
+  };
+
   return (
     <React.Fragment>
+      {console.log(cartItems)}
+
       <div className="shopping-cart-body">
         <button
           onClick={() => {
@@ -66,14 +85,24 @@ export default function CartPage() {
             setTotal={setTotal}
           />
         </div>
-        <div className="shopping-cart-totalPrice">
-          <p>Total Price: €{totalPrice.toFixed(2)}</p>
-        </div>
-        <div className="shopping-cart-actions">
-          <button className="cart-action-btn check-out" onClick={() => {
-            navigate("/order", { state: cartItems });
-          } }>Check Out</button>
-        </div>
+        {cartItems.length === 0 && (
+          <p className="cart-empty-message">There is no item in your cart. Add something from the store</p>
+        )}
+        {cartItems.length !== 0 && (
+          <div>
+            <div className="shopping-cart-totalPrice">
+              <p>Total Price: €{totalPrice.toFixed(2)}</p>
+            </div>
+            <div className="shopping-cart-actions">
+              <button
+                className="cart-action-btn check-out"
+                onClick={checkoutHandler}
+              >
+                Check Out
+              </button>
+            </div>
+          </div>
+        )}
       </div>
       <Footer />
     </React.Fragment>
